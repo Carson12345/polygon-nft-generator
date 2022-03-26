bd373b5b74a6ce05ebb34a88960c0b85a8675f80
Carson Locke Ross <carson.yaumy@gmail.com>
2022-03-26 21:01:44 +0800

    chore: add post routes

--- server/router.js
+++ server/router.js
@@ -5,8 +5,12 @@ const router = express.Router();
 
 // Get all posts
 router.get("/nft/contract/create", nft.deployNFTContract);
+router.post("/nft/contract/create", nft.deployNFTContract);
 router.get("/nft/contract/mint", nft.mintToAddressByContract);
+router.post("/nft/contract/mint", nft.mintToAddressByContract);
 router.get("/nft/contract/deploy-and-mint-and-give", nft.deployThenMintThenGive);
+router.post("/nft/contract/deploy-and-mint-and-give", nft.deployThenMintThenGive);
 router.get("/nft/contract/token-details", nft.getTokenURIByContractAndTokenId);
+router.post("/nft/contract/token-details", nft.getTokenURIByContractAndTokenId);
 
 module.exports = router;
